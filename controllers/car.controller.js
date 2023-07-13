25eed6355b7b24734556abad3487a5eae6988f09
Harris Nguyen <hainx1610@gmail.com>
2023-07-13 08:12:15 +0700

    create editCar and deleteCar for car.controller

--- controllers/car.controller.js
+++ controllers/car.controller.js
@@ -58,60 +58,44 @@ carController.getCars = async (req, res, next) => {
   }
 };
 
-// carController.getCars = async (req, res, next) => {
-// 	try {
-// 		const page = parseInt(req.query.page) || 1;
-// 		const limit = req.query.limit || 10;
-// 		const cars = await Car.find()
-// 			.sort({ createdAt: -1 })
-// 			.skip((page - 1) * limit)
-// 			.limit(limit);
-// 		const total = await Car.countDocuments({ isDeleted: false });
-// 		return res.status(200).json({ message: 'Get Car List Successfully!', cars, page, total: Math.ceil(total / limit) });
-// 	} catch (err) {
-// 		res.status(400).send({ message: err.message });
-// 	}
-// };
-
 carController.editCar = async (req, res, next) => {
   try {
     // YOUR CODE HERE
+    const { id } = req.params; // params is the id
+
+    if (!mongoose.isValidObjectId(id)) throw new Error("Invalid ID");
+
+    const car = await Car.findByIdAndUpdate(
+      id,
+      { ...req.body },
+      { new: true, runValidators: true }
+    );
+    if (!car) throw new Error("Car not found!");
+    return res.status(200).send({ message: "Update Car Successfully!", car });
   } catch (err) {
     // YOUR CODE HERE
+    res.status(400).send({ message: err.message });
   }
 };
-
-// carController.editCar = async (req, res, next) => {
-// 	try {
-// 		const { id } = req.params;
-// 		if (!mongoose.isValidObjectId(id)) throw new Error('Invalid ID');
-
-// 		const car = await Car.findByIdAndUpdate(id, { ...req.body }, { new: true, runValidators: true });
-// 		if (!car) throw new Error('Car not found!');
-// 		return res.status(200).send({ message: 'Update Car Successfully!', car });
-// 	} catch (err) {
-// 		res.status(400).send({ message: err.message });
-// 	}
-// };
 
 carController.deleteCar = async (req, res, next) => {
   try {
     // YOUR CODE HERE
+    const { id } = req.params; // params is the id
+
+    if (!mongoose.isValidObjectId(id)) throw new Error("Invalid ID");
+
+    const car = await Car.findByIdAndUpdate(
+      id,
+      { isDeleted: true },
+      { new: true, runValidators: true }
+    );
+    if (!car) throw new Error("Car not found!");
+    return res.status(200).send({ message: "Delete Car Successfully!", car });
   } catch (err) {
     // YOUR CODE HERE
+    res.status(400).send({ message: err.message });
   }
 };
-
-// carController.deleteCar = async (req, res, next) => {
-// 	try {
-// 		const { id } = req.params;
-// 		if (!mongoose.isValidObjectId(id)) throw new Error('Invalid ID');
-// 		const car = await Car.findByIdAndUpdate(id, { isDeleted: true }, { new: true, runValidators: true });
-// 		if (!car) throw new Error('Car not found!');
-// 		return res.status(200).send({ message: 'Delete Car Successfully!', car });
-// 	} catch (err) {
-// 		res.status(400).send({ message: err.message });
-// 	}
-// };
 
 module.exports = carController;
